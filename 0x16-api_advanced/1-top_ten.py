eff9bce5525a86a47ff97004855eef16f53cb61b
samuelsrmv <samuel.srmv@gmail.com>
2021-05-17 19:01:55 -0500

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,14 +5,11 @@ import requests
 
 def top_ten(subreddit):
     """Write a function that queries the Reddit API"""
-
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     req = requests.get(url)
-
     if (req.status_code != 200):
         print(None)
         return
-
     try:
         childrens = req.json().get('data').get('children')
         for children in childrens:
