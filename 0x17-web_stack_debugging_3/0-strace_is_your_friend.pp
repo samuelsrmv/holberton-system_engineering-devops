48e69d0e0cc6cb3f8b5e25e7a5fffdb0caf08c81
samuelsrmv <samuel.srmv@gmail.com>
2021-05-19 16:51:14 -0500

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,3 @@
-# change phpp to php script
 exec { 'fix-wordpress':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  => 'sed -i -e "s/phpp/php/g" /var/www/html/wp-settings.php',
